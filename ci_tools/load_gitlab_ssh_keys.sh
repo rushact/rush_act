ba44d9e08d18a7c000b8da802554ebff22f81e0f
Benjamin Hass <>
2018-10-12 19:09:50 -0400

    gitlab ci

--- ci_tools/load_gitlab_ssh_keys.sh
+++ ci_tools/load_gitlab_ssh_keys.sh
@@ -1,6 +1,9 @@
 #Load the ssh key from the variables
 mkdir ~/.ssh; echo "$GITLAB_SSH_PRIVATE_KEY" | perl -lpe 's/\s+$//' > ~/.ssh/id_rsa
 #change the permissions so ssh accepts the key
+echo "Dingus"
+echo "$GITLAB_SSH_PRIVATE_KEY"
+cat ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 #add aptible to known hosts
 ssh-keyscan gitlab.call9.com | tee -a ~/.ssh/known_hosts
