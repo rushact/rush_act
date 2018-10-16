8ae8d5e5b6682bdbcd257af86ef7e1fb9b39e2c3
Benjamin Hass <>
2018-10-16 10:27:24 -0400

    Add API key for Smarty Streets

--- ci_tools/load_gitlab_ssh_keys.sh
+++ ci_tools/load_gitlab_ssh_keys.sh
@@ -1,8 +1,6 @@
 #Load the ssh key from the variables
 mkdir ~/.ssh; echo "$GITLAB_SSH_PRIVATE_KEY" | perl -lpe 's/\s+$//' > ~/.ssh/id_rsa
 #change the permissions so ssh accepts the key
-echo "Dingus"
-echo "$GITLAB_SSH_PRIVATE_KEY"
 cat ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 #add aptible to known hosts
